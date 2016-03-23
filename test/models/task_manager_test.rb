c8d5d34362c96e44d34e44413049bec74f99c286
Jared Roth <jared.roth@gmail.com>
2016-03-23 12:05:27 -0600

    Refactored tests to be more streamlined / thourough

--- test/models/task_manager_test.rb
+++ test/models/task_manager_test.rb
@@ -3,77 +3,49 @@ require_relative '../test_helper'
 class TaskManagerTest < Minitest::Test
   include TestHelpers
   def test_it_creates_a_task
-    task_manager.create({
-      :title       => "a title",
-      :description => "a description"
-    })
+    create_tasks(1)
 
     task = task_manager.find(1)
-    assert_equal "a title", task.title
-    assert_equal "a description", task.description
+    assert_equal "Task Title 1", task.title
+    assert_equal "Task Description 1", task.description
     assert_equal 1, task.id
   end
 
   def test_it_returns_all_tasks
-    task_manager.create({
-      :title       => "a title",
-      :description => "a description"
-    })
-    task_manager.create({
-      :title       => "another title",
-      :description => "another description"
-    })
-    task_manager.create({
-      :title       => "a third title",
-      :description => "a third description"
-    })
+    create_tasks(3)
 
     all = task_manager.all
 
     assert_equal Array, all.class
     assert_equal 3, all.length
     assert_equal Task, all[0].class
-    assert_equal "a title", all[0].title
-    assert_equal "another title", all[1].title
-    assert_equal "a third title", all[2].title
+    assert_equal "Task Title 1", all[0].title
+    assert_equal "Task Title 2", all[1].title
+    assert_equal "Task Title 3", all[2].title
   end
 
   def test_it_finds_a_specific_task
-    task_manager.create({
-      :title       => "a title",
-      :description => "a description"
-    })
-    task_manager.create({
-      :title       => "another title",
-      :description => "another description"
-    })
-    task_manager.create({
-      :title       => "a third title",
-      :description => "a third description"
-    })
+    create_tasks(3)
 
     task = task_manager.find(1)
-    assert_equal "a title", task.title
-    assert_equal "a description", task.description
+    assert_equal "Task Title 1", task.title
+    assert_equal "Task Description 1", task.description
 
     task = task_manager.find(2)
-    assert_equal "another title", task.title
-    assert_equal "another description", task.description
+    assert_equal "Task Title 2", task.title
+    assert_equal "Task Description 2", task.description
 
     task = task_manager.find(3)
-    assert_equal "a third title", task.title
-    assert_equal "a third description", task.description
+    assert_equal "Task Title 3", task.title
+    assert_equal "Task Description 3", task.description
   end
 
   def test_it_updates_a_task
-    task_manager.create({
-      :title       => "a title",
-      :description => "a description"
-    })
+    create_tasks(1)
 
     before = task_manager.find(1)
-    assert_equal "a title", before.title
-    assert_equal "a description", before.description
+    assert_equal "Task Title 1", before.title
+    assert_equal "Task Description 1", before.description
 
     changes = {:title => "a changed title", :description => "a changed description"}
     task_manager.update(1, changes)
@@ -83,30 +55,19 @@ class TaskManagerTest < Minitest::Test
     assert_equal "a changed description", after.description
   end
 
-  def test_it_deletes_a_task
-    task_manager.create({
-      :title       => "a title",
-      :description => "a description"
-    })
-    task_manager.create({
-      :title       => "another title",
-      :description => "another description"
-    })
-    task_manager.create({
-      :title       => "a third title",
-      :description => "a third description"
-    })
+  def test_it_destroys_a_task
+    create_tasks(3)
 
     all = task_manager.all
     assert_equal 3, all.length
-    assert_equal "a third title", all[2].title
-    assert_equal "another title", all[1].title
+    assert all.any?{|task| task.title == "Task Title 3"}
+    assert all.any?{|task| task.title == "Task Title 2"}
 
     task_manager.destroy(2)
     all = task_manager.all
     assert_equal 2, all.length
-    assert_equal "a third title", all[1].title
-    assert_equal "a title", all[0].title
+    assert all.any?{|task| task.title == "Task Title 3"}
+    refute all.any?{|task| task.title == "Task Title 2"}
 
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,13 @@ module TestHelpers
     database = YAML::Store.new('db/task_manager_test')
     @task_manager ||= TaskManager.new(database)
   end
+
+  def create_tasks(num = 2)
+    num.times do |i|
+      task_manager.create({
+        :title => "Task Title #{i + 1}",
+        :description => "Task Description #{i + 1}"
+        })
+    end
+  end
 end
